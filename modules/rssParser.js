fb56f4bdcd1b3fa04387e3cc7a7a2cff364d9b64
Andres Osorio Plata <and285@gmail.com>
2015-05-10 14:36:00 -0500

    feedParser typo fix for travis ci build

--- modules/rssParser.js
+++ modules/rssParser.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var cheerio = require('cheerio');
-var FeedParser = require('feedParser');
+var FeedParser = require('feedparser');
 var http = require('http');
 var PromisePolyfill = require('Promise');
 var urlEncode = require('urlEncode');
